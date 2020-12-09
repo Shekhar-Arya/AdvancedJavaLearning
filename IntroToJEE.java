8dd132923d5caa5cbf4e06fb95b0f2ea082360f9
Shekhar Arya <9925648129shekhar@gmail.com>
2020-12-09 15:41:40 +0530

    Intro to the advance java topics.

---
+++ IntroToJEE.java
@@ -0,0 +1,22 @@
+public class IntroToJEE {
+    public static void main(String[] args) {
+        
+        // JEE means Java Enterprise Edition
+        // This is not an intallable software, it is a rules and regulation based software,
+        // that is used to create web server software(Eg. Tomcat server) or
+        // application server software(Eg. weblogic, websphere, servelet jsp).
+        // Working with these web server or application server software which are installable,
+        // is nothing but working with the JEE.
+        
+        // JEE is used to develop (Web application and Distributed application)
+        // Now Web application is client-server application.
+        // Here client is a browser which send the requests and get the respose and display it.
+        // Wether server is the website which capture the request and give the respnse to the client.
+        // Thats y it is also known as Thin Client Fat Server application.
+
+        // Distributed application is also client-server application.
+        // where client is software application and server is also software application.
+        // and in this load is distributed among the client and server so thats y it is known as distrubuted application.
+        
+    }
+}
---
+++ IntroToJSE.java
@@ -0,0 +1,29 @@
+public class IntroToJSE {
+    public static void main(String[] args) {
+        
+
+        // JSE is nothing but installable software as JDK.
+        // This is usefull for Standalone apps and applets
+        // Now if we talk about Standalone apps then this are application that will run only for wherever it is installed.
+        // Eg. like antivirus or single player game.
+        // There are two types of Standalone apps.
+        // A) CUI (Character User Interface) app /Console app,  java class is example
+        // B) GUI (Graphic User Interface) app /Desktop app, any app with buttons and lables and all are example.
+        // Now if we talk about applets than this are java based web pages.
+        // Like java classes that can be sent over to the network.
+        // HTML and applets are different. There is less security in HTML but easy to use.
+        // And There is more security in applets but not easy to use as HTML.
+        // There are two type of applets.
+        // 1) trusted applet -This applet when downloaded from network,
+        // It cannot read from our system or it cannot write any thing to our system.
+        // So There is no way that virus can be read to that applet or can be read from that applet.
+        // 2) untrusted applet - This is reverse of the trusted applet.
+
+        // Alterantive for JSE Modules are:- VB, VB.net, D2K, Fox Pro and etc.
+
+        // Core Java is Basically the initiall part of the JSE module language part.
+        // And Advance Java is the technology part of the JSE module and initial technology of JEE.
+        // Technology like JDBC, servelet, nd etc.  
+
+    }
+}
